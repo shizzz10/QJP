3c6bb203544e38aa412e4b977b34c8d71d7d45b7
Sheraz Khan <skwtanai5@gmail.com>
2024-12-11 04:29:44 +0500

    improved professional resume page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+    "./index.html",      // Specifically match index.html in the root directory
+    "./src/**/*.{html,js}" // Match HTML and JS files in the src folder and its subfolders
+  ],
   theme: {
     extend: {},
   },
